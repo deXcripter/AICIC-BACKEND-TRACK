4b902078415790b90103fb9e1333c6d4587a568c
johnpaul <dexcripter@gmail.com>
2025-10-17 09:00:09 +0100

    routing with express

--- index.js
+++ index.js
@@ -1,69 +1,61 @@
-const http = require("http");
+const express = require("express");
 const fs = require("fs");
-const url = require("url");
 
-let posts = fs.readFileSync("./dbs/posts.json", "utf-8");
+const app = express();
+const posts = JSON.parse(fs.readFileSync("./dbs/posts.json", "utf8"));
 
-const server = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url)
+// middleware
+app.use(express.json());
 
+// Controllers
+const getAllPosts = (req, res) => {
+  res.send(posts);
+};
 
+function createPost(req, res) {
+  // a user account was created
 
+  const newPost = req.body;
+  newPost.id = posts.length + 1;
 
-  // Get all posts
-  if (req.url === "/posts" && req.method === "GET") {
-    res.end(posts);
-    return;
-  } else if (req.url === "/posts" && req.method === "POST") {
-    let posts = fs.readFileSync("./dbs/posts.json", "utf-8");
+  posts.shift(newPost);
 
-    let body = "";
+  fs.writeFileSync("./dbs/posts.json", JSON.stringify(posts));
 
-    req.on("data", (packet) => {
-      body += packet.toString();
-    });
+  res.status(201).send(newPost);
+}
 
-    // When all data has been received
-    req.on("end", () => {
-      try {
-        const existingPosts = JSON.parse(posts);
-        const newPost = JSON.parse(body);
-        const formattedPost = {
-          id: existingPosts.length + 1,
-          ...newPost,
-        };
-        existingPosts.push(formattedPost);
+function getPost(req, res) {
+  const identifier = parseInt(req.params.id);
 
-        fs.writeFileSync("./dbs/posts.json", JSON.stringify(existingPosts));
-
-
-        res.writeHead(201, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({
-            success: true,
-            message: "Post created successfully",
-          })
-        );
-      } catch (error) {
-        // Handle JSON parsing errors
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(
-          JSON.stringify({
-            success: false,
-            message: "Invalid JSON data",
-          })
-        );
-      }
-    });
-  } else if (req.url === "/posts" && req.method === "GET") {
-
-    console.log(req.params)
-
+  if (identifier > posts.length) {
+    return res.status(404).json({ message: "This post does not exist" });
   }
 
-  res.end("fallback")
+  const post = posts.find((elem) => elem.id === identifier);
+
+  res.send({
+    post: post,
+  });
+}
+
+function deletePost(req, res) {}
+app.delete("/api/v1/posts/:id", (req, res) => {
+  // Leave this for now
+  res.status(501).json({ message: "Route not implemented" });
 });
 
-server.listen(8000, () => {
-  console.log("Server is litening on port 8000");
+app.patch("/api/v1/posts/:id", (req, res) => {
+  // Leave this for now
+  res.status(501).json({ message: "Route not implemented" });
+});
+
+// Routes handlers
+app.get("/api/v1/posts", getAllPosts);
+app.post("/api/v1/posts", createPost);
+app.get("/api/v1/posts/:id", getPost);
+
+const port = 9000;
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}..`);
 });
